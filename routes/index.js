43e891ca3ea0ac6e5ba20dfd2a47a56a366c4d6e
Устинов Алексей Геннадьевич <ustinov@nports.ru>
2016-09-14 15:11:42 +0500

    !__!

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
-// let main = require('./api');
+let users = require('../users');
 let express = require('express');
 
 module.exports = function (app) {
     app.use(express.static('public'));
+
+    app.get('/api/users', function(req, res){
+       res.send(users());
+
+    });
 
     // app.get('*', error['404']);
 };
---
+++ users/index.js
@@ -0,0 +1,8 @@
+var User = function(){
+
+    return '{"data":"some-data2"}'
+};
+
+
+
+module.exports = User;
