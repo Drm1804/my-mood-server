23baf7470afa11d4680099916ecf525ee18c2ec7
Drm1804 <drm-a@yandex.ru>
2016-09-20 20:01:52 +0500

    Разобрался как парсить post запросы

--- routes/index.js
+++ routes/index.js
@@ -33,27 +33,31 @@ module.exports = function (app) {
     /** Добавить mood*/
     app.post('/api/mood', function(req, res) {
         // debugger;
-        let mood = new MoodModel({
-            name: req.body.title,
-            score: req.body.score,
-        });
 
-        mood.save(function (err) {
-            if (!err) {
-                log.info("article created");
-                return res.send({ status: 'OK', article:mood });
-            } else {
-                console.log(err);
-                if(err.name == 'ValidationError') {
-                    res.statusCode = 400;
-                    res.send({ error: 'Validation error' });
-                } else {
-                    res.statusCode = 500;
-                    res.send({ error: 'Server error' });
-                }
-                log.error('Internal error(%d): %s',res.statusCode,err.message);
-            }
-        });
+        console.dir(req.body);
+        // let mood = new MoodModel({
+        //     name: req.body.title,
+        //     score: req.body.score,
+        // });
+
+        // mood.save(function (err) {
+        //     if (!err) {
+        //         log.info("article created");
+        //         return res.send({ status: 'OK', article:mood });
+        //     } else {
+        //         console.log(err);
+        //         if(err.name == 'ValidationError') {
+        //             res.statusCode = 400;
+        //             res.send({ error: 'Validation error' });
+        //         } else {
+        //             res.statusCode = 500;
+        //             res.send({ error: 'Server error' });
+        //         }
+        //         log.error('Internal error(%d): %s',res.statusCode,err.message);
+        //     }
+        // });
+
+        return res.send({ status: 'OK', article:'mood' });
     });
 
     /** Получить один mood*/
--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 let express = require('express');
+let bodyParser = require('body-parser');
 let app = express();
 let log = require('./libs/log')(module);
 // let middleware = require('./middleware')(app, express);
 let config = require('./config');
 let routes = require('./routes');
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
 
+    next();
+};
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(allowCrossDomain);
 routes(app);
 app.listen(config.get('port'), function () {
     console.log(`Example app listening on port ${config.get('port')}!`);
