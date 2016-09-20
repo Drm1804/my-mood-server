b9b2ca0601f3f181f0f5420e405b724548701525
Drm1804 <drm-a@yandex.ru>
2016-09-20 20:22:46 +0500

    Сделал добавление mood

--- routes/index.js
+++ routes/index.js
@@ -32,32 +32,31 @@ module.exports = function (app) {
 
     /** Добавить mood*/
     app.post('/api/mood', function(req, res) {
-        // debugger;
 
-        console.dir(req.body);
-        // let mood = new MoodModel({
-        //     name: req.body.title,
-        //     score: req.body.score,
-        // });
 
-        // mood.save(function (err) {
-        //     if (!err) {
-        //         log.info("article created");
-        //         return res.send({ status: 'OK', article:mood });
-        //     } else {
-        //         console.log(err);
-        //         if(err.name == 'ValidationError') {
-        //             res.statusCode = 400;
-        //             res.send({ error: 'Validation error' });
-        //         } else {
-        //             res.statusCode = 500;
-        //             res.send({ error: 'Server error' });
-        //         }
-        //         log.error('Internal error(%d): %s',res.statusCode,err.message);
-        //     }
-        // });
+        let mood = new MoodModel({
+            name: req.body.title,
+            score: req.body.score,
+        });
 
-        return res.send({ status: 'OK', article:'mood' });
+        mood.save(function (err) {
+        console.log(err);
+            if (!err) {
+                log.info("article created");
+                return res.send({ status: 'OK', article:mood });
+            } else {
+                console.log(err);
+                if(err.name == 'ValidationError') {
+                    res.statusCode = 400;
+                    res.send({ error: 'Validation error' });
+                } else {
+                    res.statusCode = 500;
+                    res.send({ error: 'Server error' });
+                }
+                log.error('Internal error(%d): %s',res.statusCode,err.message);
+            }
+        });
+
     });
 
     /** Получить один mood*/
